6b2c3fea38476a85b9ac7ba835551a9743df5998
sananes <aasananes@gmail.com>
2020-12-27 14:29:39 +0000

    refactor(update): Refactored and downgrade slider

--- web/src/sections/Testimonial.vue
+++ web/src/sections/Testimonial.vue
@@ -1,38 +1,40 @@
 <template>
   <div class="testimonial">
     <div class="container--narrow">
-      <swiper ref="mySwiper" :options="swiperOptions">
-        <swiper-slide v-for="(testimonial, index) in testimonials" :key="index">
-          <div class="testimonial__rating">
-            <icon name="star" />
-            <icon name="star" />
-            <icon name="star" />
-            <icon name="star" />
-            <icon name="star" />
-          </div>
-          <blockquote class="testimonial__quote">{{ testimonial.text }}</blockquote>
-          <aside class="testimonial__author" v-if="testimonial.name">
-            <strong class="testimonial__name">{{ testimonial.name }}</strong
-            ><span class="testimonial__role" v-html="testimonial.role" /></aside
-        ></swiper-slide>
-        <div class="swiper-pagination" slot="pagination"></div>
-      </swiper>
+      <client-only
+        ><swiper class="swiper" :options="swiperOption">
+          <swiper-slide v-for="(testimonial, index) in testimonials" :key="index">
+            <div class="testimonial__rating">
+              <icon name="star" />
+              <icon name="star" />
+              <icon name="star" />
+              <icon name="star" />
+              <icon name="star" />
+            </div>
+            <blockquote class="testimonial__quote">{{ testimonial.text }}</blockquote>
+            <aside class="testimonial__author" v-if="testimonial.name">
+              <strong class="testimonial__name">{{ testimonial.name }}</strong
+              ><span class="testimonial__role" v-html="testimonial.role" /></aside
+          ></swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+      </client-only>
     </div>
   </div>
 </template>
 
 <script>
 import Icon from '../components/Icon.vue'
-import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
-import 'swiper/swiper-bundle.css'
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 export default {
   data() {
     return {
-      swiperOptions: {
+      swiperOption: {
         pagination: {
           el: '.swiper-pagination',
+          dynamicBullets: true,
         },
-        // Some Swiper option/callback...
       },
       testimonials: [
         {
