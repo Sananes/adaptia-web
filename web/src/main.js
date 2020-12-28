ed78cc8dca81d5d68dd9cad74d038b6506514ce4
sananes <aasananes@gmail.com>
2020-12-28 07:33:40 +0000

    Updated package

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,7 @@
 // Import main css
 import '~/assets/style/index.scss'
-import Vue from 'vue'
-import '@animxyz/core'
 import VueAnimXYZ from '@animxyz/vue'
+import '@animxyz/core/dist/animxyz.min.css'
 
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from '~/layouts/Default.vue'
