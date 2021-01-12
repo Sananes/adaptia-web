4aba9d28dc8788886d687726c7fc7a502bb5a435
sananes <aasananes@gmail.com>
2021-01-12 15:01:38 +0000

    Removed Roobert font

--- web/src/layouts/Default.vue
+++ web/src/layouts/Default.vue
@@ -21,82 +21,82 @@ export default {
 <style lang="scss">
 @font-face {
   font-family: Tungsten;
-  src: url('../assets/fonts/Tungsten.woff'),
-    url('../assets/fonts/Tungsten.woff2') format('woff2'),
+  src: url('../assets/fonts/Tungsten.woff'), url('../assets/fonts/Tungsten.woff2') format('woff2'),
     url('../assets/fonts/Tungsten.eot') format('eot'),
     url('../assets/fonts/Tungsten.eot?#iefix') format('embedded-opentype'),
     url('../assets/fonts/Tungsten.ttf') format('truetype');
+  font-display: swap;
   font-weight: normal;
   font-style: normal;
 }
 
-@font-face {
-  font-family: Roobert;
-  src: url('../assets/fonts/Roobert.woff') format('woff'),
-    url('../assets/fonts/Roobert.woff2') format('woff2');
-  font-weight: normal;
-  font-display: swap;
-}
+// @font-face {
+//   font-family: Roobert;
+//   src: url('../assets/fonts/Roobert.woff') format('woff'),
+//     url('../assets/fonts/Roobert.woff2') format('woff2');
+//   font-weight: normal;
+//   font-display: swap;
+// }
 
-@font-face {
-  font-family: Roobert;
-  src: url('../assets/fonts/Roobert-Italic.woff') format('woff'),
-    url('../assets/fonts/Roobert-Italic.woff2') format('woff2');
-  font-weight: normal;
-  font-style: italic;
-  font-display: swap;
-}
+// @font-face {
+//   font-family: Roobert;
+//   src: url('../assets/fonts/Roobert-Italic.woff') format('woff'),
+//     url('../assets/fonts/Roobert-Italic.woff2') format('woff2');
+//   font-weight: normal;
+//   font-style: italic;
+//   font-display: swap;
+// }
 
-@font-face {
-  font-family: Roobert;
-  src: url('../assets/fonts/Roobert-Medium.woff') format('woff'),
-    url('../assets/fonts/Roobert-Medium.woff2') format('woff2');
-  font-weight: 500;
-  font-display: swap;
-}
+// @font-face {
+//   font-family: Roobert;
+//   src: url('../assets/fonts/Roobert-Medium.woff') format('woff'),
+//     url('../assets/fonts/Roobert-Medium.woff2') format('woff2');
+//   font-weight: 500;
+//   font-display: swap;
+// }
 
-@font-face {
-  font-family: Roobert;
-  src: url('../assets/fonts/Roobert-Medium-Italic.woff') format('woff'),
-    url('../assets/fonts/Roobert-Medium-Italic.woff2') format('woff2');
-  font-weight: 500;
-  font-style: italic;
-  font-display: swap;
-}
+// @font-face {
+//   font-family: Roobert;
+//   src: url('../assets/fonts/Roobert-Medium-Italic.woff') format('woff'),
+//     url('../assets/fonts/Roobert-Medium-Italic.woff2') format('woff2');
+//   font-weight: 500;
+//   font-style: italic;
+//   font-display: swap;
+// }
 
-@font-face {
-  font-family: Roobert;
-  src: url('../assets/fonts/Roobert-Semibold.woff') format('woff'),
-    url('../assets/fonts/Roobert-Semibold.woff2') format('woff2');
-  font-weight: 600;
-  font-display: swap;
-}
+// @font-face {
+//   font-family: Roobert;
+//   src: url('../assets/fonts/Roobert-Semibold.woff') format('woff'),
+//     url('../assets/fonts/Roobert-Semibold.woff2') format('woff2');
+//   font-weight: 600;
+//   font-display: swap;
+// }
 
-@font-face {
-  font-family: Roobert;
-  src: url('../assets/fonts/Roobert-Semibold-Italic.woff') format('woff'),
-    url('../assets/fonts/Roobert-Semibold-Italic.woff2') format('woff2');
-  font-weight: 600;
-  font-style: italic;
-  font-display: swap;
-}
+// @font-face {
+//   font-family: Roobert;
+//   src: url('../assets/fonts/Roobert-Semibold-Italic.woff') format('woff'),
+//     url('../assets/fonts/Roobert-Semibold-Italic.woff2') format('woff2');
+//   font-weight: 600;
+//   font-style: italic;
+//   font-display: swap;
+// }
 
-@font-face {
-  font-family: Roobert;
-  src: url('../assets/fonts/Roobert-Bold.woff') format('woff'),
-    url('../assets/fonts/Roobert-Bold.woff2') format('woff2');
-  font-weight: 700;
-  font-display: swap;
-}
+// @font-face {
+//   font-family: Roobert;
+//   src: url('../assets/fonts/Roobert-Bold.woff') format('woff'),
+//     url('../assets/fonts/Roobert-Bold.woff2') format('woff2');
+//   font-weight: 700;
+//   font-display: swap;
+// }
 
-@font-face {
-  font-family: Roobert;
-  src: url('../assets/fonts/Roobert-Bold-Italic.woff') format('woff'),
-    url('../assets/fonts/Roobert-Bold-Italic.woff2') format('woff2');
-  font-weight: 700;
-  font-style: italic;
-  font-display: swap;
-}
+// @font-face {
+//   font-family: Roobert;
+//   src: url('../assets/fonts/Roobert-Bold-Italic.woff') format('woff'),
+//     url('../assets/fonts/Roobert-Bold-Italic.woff2') format('woff2');
+//   font-weight: 700;
+//   font-style: italic;
+//   font-display: swap;
+// }
 
 .container {
   margin: 0 auto;
